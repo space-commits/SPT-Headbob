2edd70947440a60ac61ab7a1f95273efce6b028f
space-commits <57108281+space-commits@users.noreply.github.com>
2023-01-23 17:06:09 +0000

    Update to 3.5.0

--- Patches.cs
+++ Patches.cs
@@ -36,26 +36,28 @@ namespace Headbob
         }
     }
 
+
+    //to find this method for a new client version, look for something like this: this.HeadBobbing = base.method_2<float>("Settings/Game/HeadBobbing", 0.2f, new Func<float, float>(GClass971.Class1513.class1513_0.method_1));
     public class HeadBobClampPatch : ModulePatch
     {
 
         private static Type _targetType;
-        private static MethodInfo _method0;
+        private static MethodInfo _method_1;
 
         public HeadBobClampPatch()
         {
             _targetType = PatchConstants.EftTypes.Single(isType);
-            _method0 = _targetType.GetMethod("method_1", BindingFlags.FlattenHierarchy | BindingFlags.NonPublic | BindingFlags.Instance);
+            _method_1 = _targetType.GetMethod("method_1", BindingFlags.FlattenHierarchy | BindingFlags.NonPublic | BindingFlags.Instance);
         }
 
         public static bool isType(Type type)
         {
-            return type.GetField("class1364_0") != null && type.GetField("func_0") != null;
+            return type.GetField("class1513_0") != null && type.GetField("func_0") != null;
         }
 
         protected override MethodBase GetTargetMethod()
         {
-            return _method0;
+            return _method_1;
         }
 
         [PatchPrefix]
