665216e2f0371176affc7ccdabf56305e41ba21b
Hauzmanus <vhauzman@gmail.com>
2025-04-16 09:16:16 +0200

    3.11 Update
    Updated GClasses, rewrote some code to make the mod work for 3.11

---
+++ HeadbobPatch/HeadbobReducer.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using EFT.UI;
+using EFT.UI.Settings;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace Headbob.ExamplePatches
+{
+    [BepInPlugin("hauzman.bob", "Hauzman.Headbobbing", "1.0.0")]
+    public class HeadBobRange : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GameSettingsTab), "Show");
+        }
+        [PatchPostfix]
+        private static void PatchPostfix(ref NumberSlider ____headbobbing, GClass1053 ___gclass1053_0)
+        {
+            SettingsTab.BindNumberSliderToSetting(____headbobbing, ___gclass1053_0.HeadBobbing, 0f, 1f, "F1");
+        }
+    }
+    public class HeadBobPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(GClass1053.Class1718).GetMethod("method_1", BindingFlags.Public | BindingFlags.Instance);
+        }
+        [PatchPrefix]
+        private static bool Prefix(ref float __result, float x)
+        {
+            __result = Mathf.Clamp(x, 0.0f, 1f);
+            return false;
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,15 +1,29 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Headbob.ExamplePatches;
 
 namespace Headbob
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    // first string below is your plugin's GUID, it MUST be unique to any other mod. Read more about it in BepInEx docs. Be sure to update it if you copy this project.
+    [BepInPlugin("Headbob.UniqueGUID", "Headbob", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
+        public static ManualLogSource LogSource;
+
+        // BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
         private void Awake()
         {
-            new HeadBobRangePatch().Enable();
-            new HeadBobClampPatch().Enable();
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            // save the Logger to variable so we can use it elsewhere in the project
+            LogSource = Logger;
+            LogSource.LogInfo("plugin loaded!");
+            // uncomment line(s) below to enable desired example patch, then press F6 to build the project:
+            new HeadBobRange().Enable();
+            new HeadBobPatch().Enable();
         }
     }
 }
